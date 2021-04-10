48c833351cacd46250f6c5546df653bb097c32d2
yashvantgohil <yashvantgohil.dev@gmail.com>
2021-04-10 14:26:18 +0530

    Added Angular Material Created Comomn Material Component Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialComponentsModule } from './shared/module/angular-material/angular-material-components.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/module/angular-material/angular-material-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule
+  ],
+  exports:[
+    MatButtonModule,
+    MatCardModule
+  ]
+})
+export class AngularMaterialComponentsModule { }
